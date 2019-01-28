354a95e07132336a59d810f8eb5bb8923ff6f92c
Sai Madhav Perumalla <sai.madhav@scroll.network>
2019-01-28 16:53:47 -0500

    Fixes

--- src/ngx-drag-scroll.ts
+++ src/ngx-drag-scroll.ts
@@ -40,7 +40,7 @@ import { DragScrollItemDirective } from './ngx-drag-scroll-item';
       background: white;
     }
    .drag-scroll-content::-webkit-scrollbar-thumb {
-    background-color: #E0E0E0;;
+    background-color: #E0E0E0;
     outline: 1px solid slategrey;
     }
     `]
